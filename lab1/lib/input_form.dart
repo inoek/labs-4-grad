8e14b3f7f41e9d3437056cb9506e8d57d3a1e6b8
Igor Noek <noekihar@gmail.com>
2021-12-05 20:25:54 +0300

    save form

--- lab1/lib/input_form.dart
+++ lab1/lib/input_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/input_form_vm.dart';
+import 'package:lab1/models.dart';
+import 'package:provider/provider.dart';
 
 class InputForm extends StatelessWidget {
   InputForm({Key? key}) : super(key: key);
@@ -14,17 +17,18 @@ class InputForm extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(24),
         children: [
-          _buildForm(),
+          _buildForm(context),
           const SizedBox(height: 24),
           ElevatedButton(
-              onPressed: () => _formKey.currentState?.save(),
-              child: Text('Save form'))
+            onPressed: () => _onSave(context),
+            child: Text('Save form'),
+          )
         ],
       ),
     );
   }
 
-  Widget _buildForm() {
+  Widget _buildForm(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -35,16 +39,22 @@ class InputForm extends StatelessWidget {
         const SizedBox(height: 10),
         TextFormField(
           decoration: InputDecoration(label: Text('first name')),
+          onSaved: (value) => context.read<Student>().inputFirstName = value,
         ),
         TextFormField(
           decoration: InputDecoration(label: Text('second name')),
+          onSaved: (value) => context.read<Student>().inputSecondName = value,
         ),
         TextFormField(
           decoration: InputDecoration(label: Text('direction')),
+          onSaved: (value) => context.read<Student>().inputDirection = value,
         ),
         TextFormField(
           decoration: InputDecoration(label: Text('year')),
+          keyboardType: TextInputType.number,
+          onSaved: (value) => context.read<Student>().inputYear = value,
         ),
+        const SizedBox(height: 20),
         Text(
           'Teacher',
           style: TextStyle(fontWeight: FontWeight.bold),
@@ -64,5 +74,26 @@ class InputForm extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  void _onSave(BuildContext context) {
+    _formKey.currentState?.save();
+    final studentVM = context.read<Student>();
+    final teacherVM = context.read<Teacher>();
+    final student = Student(
+      direction: studentVM.inputDirection,
+      year: studentVM.inputYear,
+      firstName: studentVM.inputFirstName,
+      secondName: studentVM.inputSecondName,
+    );
+    final teacher = Teacher(
+      subject: teacherVM.inputSubject,
+      age: teacherVM.inputAge,
+      firstName: teacherVM.inputFirstName,
+      secondName: teacherVM.inputSecondName,
+    );
+    studentVM.addStudent(student);
+    teacherVM.addTeacher(teacher);
+    context.read<InputFormVM>().isEmpty = false;
   }
 }
